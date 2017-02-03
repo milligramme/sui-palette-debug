49dd314fd30e40b58ff99ef79d3ba0d16639d3c2
milligramme <milligramme.cc@gmail.com>
2017-02-03 10:23:27 +0900

    Add resizable, close #1

--- logger/index.jsx
+++ logger/index.jsx
@@ -10,7 +10,7 @@ var PUTS = function (args) {
 }
 
 var create_logger = function () {
-  logger = new Window('palette', "logger", u, {closeButton:true, minimizeButton:true, maximizeButton:false, resizeable:false, borderless:false});
+  logger = new Window('palette', "logger", u, {closeButton:true, minimizeButton:true, maximizeButton:false, resizeable:true, borderless:false});
   logger.orientation = 'column';
   logger.margins = 5;
   logger.spacing = 5;
@@ -20,8 +20,18 @@ var create_logger = function () {
   TEXT_AREA.preferredSize = [240,320];
   
   var clear_btn = logger.add('button', u, "Clear", {name: "clear"});
+  clear_btn.preferredSize = [240,23];
+  clear_btn.minimumSize.height = 23;
+  clear_btn.maximumSize.height = 46;
   clear_btn.onClick = function () {
     TEXT_AREA.text = "";
   }
+  
+  logger.onShow =
+  logger.onResizing =
+  logger.onResize = function() {
+    this.layout.resize();
+  }
+  
   return logger
 }
